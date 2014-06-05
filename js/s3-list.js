94350435bc45580f11d788f063013df0944960a4
Derek Eder <derek.eder@gmail.com>
2014-06-05 11:46:13 -0500

    friendly printing of file size

--- js/s3-list.js
+++ js/s3-list.js
@@ -140,7 +140,7 @@ function renderTable(info) {
 function renderRow(item, cols) {
   var row = '';
   row += padRight(moment(item.LastModified).format('MMM D, YYYY h:mma'), cols[1]) + '  ';
-  row += padRight(item.Size, cols[2]);
+  row += padRight(bytesToSize(item.Size), cols[2]);
   row += '<a href="' + BUCKET_URL + item.href + '">' + item.keyText + '</a>';
   return row;
 }
@@ -156,3 +156,10 @@ function padRight(padString, length) {
   return str;
 }
 
+function bytesToSize(bytes) {
+   if(bytes == 0) return '0 Byte';
+   var k = 1000;
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   var i = Math.floor(Math.log(bytes) / Math.log(k));
+   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+}
