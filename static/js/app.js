e4d7db7dc31b3890fa63bf8d8e0550169f3fb1f2
Derek Eder <derek.eder@gmail.com>
2014-07-16 12:35:29 -0500

    added content from #11, moved data quality info to about, reorganized home page content

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,4 @@
 (function(){
-
-    initQualityChart('quality_chart');
-
     load_list('candidates');
     load_list('committees');
     load_list('receipts');
@@ -49,61 +46,5 @@ function makeDataTable(el) {
       "bInfo": false,
       "bFilter": false,
       "bPaginate": false
-  });
-}
-
-function initQualityChart(el) {
-  $('#' + el).highcharts({
-      chart: {
-          type: 'bar'
-      },
-      title: {
-          text: null
-      },
-      xAxis: {
-        title: null,
-          labels: {
-            enabled: false
-          }
-      },
-      yAxis:{
-          title: null,
-          min: 1989,
-          max: 2015
-      },
-      plotOptions: {
-          series: {
-              stacking: 'true'
-          }
-      },
-      tooltip: {
-        borderColor: "#ccc",
-        formatter: function() {
-          return this.series.name;
-        }
-      },
-      legend: { reversed: true },
-      series: [
-        {
-          name: '2000 on: Electronic filings',
-          data: [ 15 ],
-          color: "#43ac6a",
-        },
-        {
-          name: '1999: Incomplete',
-          data: [ 1 ],
-          color: "#d9edf7"
-        },
-        {
-          name: '1994 - 1999: Manually entered',
-          data: [ 5 ],
-          color: "#43ac6a"
-        }, 
-        {
-          name: '1989 - 1994: Bad entries',
-          data: [ 1994 ],
-          color: "#d9edf7"
-        }
-      ]
   });
 }
